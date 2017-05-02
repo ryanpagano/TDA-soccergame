82d8f21b72c2f7cd99ab65cecfa6659fe4be3fcf
ryanpagano <shutdafuqupandplay@gmail.com>
2017-05-02 11:42:05 -0300

    soccer-game

---
+++ TP1 B - TDA Basico/Pelota.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_B___TDA_Basico
+{
+    class Pelota
+    {
+        public float movX;
+        public float movY;
+    }
+}
